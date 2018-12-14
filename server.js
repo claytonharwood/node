47a5d1ac1c513626fa6ab97c2f1fa5c7649935c1
Clayton Harwood <bigfatweb@gmail.com>
2018-12-13 21:14:06 -0700

    server update

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-const app = express()
+var path = require('path');
+var express = require('express');
+var app = express();
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
-app.listen(process.env.PORT || 8080, () => console.log(Up and Running!))
+app.listen(process.env.PORT || 8080, () => console.log('Up and Running!'));
