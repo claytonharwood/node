9ff351d0b9d2e74b50eb658951f66e897ea367bb
Clayton Harwood <bigfatweb@gmail.com>
2018-12-13 21:04:30 -0700

    updates

--- server.js
+++ server.js
@@ -10,13 +10,6 @@ app.set("view engine", "ejs");
 app.use(express.json() );      
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, 'public')))
-
-// index page 
-app.get('/', function(req, res) {
-    res.render('index');
-});
-
 app.listen(5000, function() {
 	console.log("The server is up and listening on port 5000");
 });
